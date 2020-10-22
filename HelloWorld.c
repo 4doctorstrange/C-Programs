2977723d6120c994a101c5fc4a0f6be20e2c8239
Salamander19-tech <58026725+Salamander19-tech@users.noreply.github.com>
2020-10-22 11:30:45 +0530

    Update HelloWorld.c
    Update it please.

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,9 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    printf("Hello World\n");
-    printf("yo\n");
-    printf("Welcome to the C Programming Language");
-    printf("This is my first C Progam")
-    return 0;
+	printf("Hello World!");
+	printf("Welcome to C Programming!");
+	return 0;
 }
